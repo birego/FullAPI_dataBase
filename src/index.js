424f5ebea6eb86d5f8048cac6d3df42f22fd6a52
BirushaNdegeya <birushandegeya@gmail.com>
2024-05-27 10:09:01 +0200

    add cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import apprenantRoutes from "./routes/apprenant.js";
 import coachRoutes from "./routes/coach.js";
 import cohorteRoutes from "./routes/cohorte.js";
@@ -6,6 +7,7 @@ import sessionRoutes from "./routes/session.js";
 import ordinateurRoutes from "./routes/ordinateur.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/apprenants", apprenantRoutes);
@@ -15,9 +17,12 @@ app.use("/sessions", sessionRoutes);
 app.use("/cohortes", cohorteRoutes);
 
 app.get("/", function (req, res) {
-  res.send("hello world");
+  res.send({ message: "server live"});
 });
 
+app.use('*', function(req, res){
+  res.json({ message: 'not found'});
+})
 app.listen(8000, function () {
   console.log(`server running at http://localhost:8000`);
 });
